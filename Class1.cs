f03018698c97e3f9dcd35d0c511467acf700b227
Peter Kun <gamerko@gmail.com>
2024-06-04 22:51:49 +0300

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,32 @@
+﻿using Allure.NUnit;
+using Allure.NUnit.Attributes;
+using NUnit.Framework;
+
+namespace allure_nunit_ignore_bug
+{
+    [AllureNUnit]
+    [AllureDisplayIgnored]
+    public class Class1
+    {
+        [Ignore("ignored")]
+        [Test]
+        public void Ignored()
+        {
+
+        }
+
+        [Ignore("ignored with cases")]
+        [TestCase(true)]
+        [TestCase(false)]
+        public void Ignored_Cases(bool tmp)
+        {
+
+        }
+
+        [Test]
+        public void Passed()
+        {
+
+        }
+    }
+}
